5a887f79045789245a265f344eae877ed2c30b4b
hwgq2005 <262281610@qq.com>
2017-02-03 23:08:07 +0800

    弹框样式修改

--- package/hbook-ui/dist/js/hbook.js
+++ package/hbook-ui/dist/js/hbook.js
@@ -5,122 +5,156 @@
  * @version 1.0
  */
 
-!function($) {
+!function(window,$) {
 
 	"use strict";
+	// 默认配置
+	
 
-	var Dialog=function(element, options){
+	window.dialogIndex = 1050;
+
+	var Dialog=function(options){
+
+		var defaults={
+			title:'提示',
+			type:0, 	    // 0:有头部和尾部 1:没有头部 2:没有尾部 3：没有头尾 
+			backdrop:true,  // 是否出现遮罩
+			confirmButton:true,  //确认按钮
+			cancelButton:true,	 //取消按钮
+			confirm:function(){},
+			cancel:function(){}
+		}
+
+		var options = $.extend(defaults, options);
 
 		this.options=options;
-		this.$element=$(element);
-		this.$backdrop = null;
+		this.confirm=options.confirm;
+		this.cancel=options.cancel;
+		this.$element=$('#'+options.id);
 		this.init(this.options,this.$element);
 		
 	}
 
 	// 初始化
-	Dialog.prototype.init=function(options,elememt){
+	Dialog.prototype.init=function(options,$element){
 
 		var _self = this;
 
-		
-		
-		options.modal == 'show' ? this.show(options,elememt) : this.hide(options,elememt);
-
-		_self.bindEvent(options,elememt);
+		if ($element.length <= 0) {
+			dialogIndex ++ ;
+			_self.show(options,$element);
+		}
 		
 	}
 
 	// 显示弹出框
-	Dialog.prototype.show=function(options,elememt){
+	Dialog.prototype.show=function(){
 		
-		this.backdrop(options,elememt)
-		this.$element.addClass('in');
+		var _self = this,
+			options = _self.options;
+
+		var typeClass = '';	
+		if (options.type == 1){
+			typeClass = 'dialog-tip' ; 
+		}
+		var _html = '<div class="dialog '+typeClass+'" id="'+options.id+'" style="z-index:'+(dialogIndex - 1)+'">';
+
+		if ( (options.type == 0 || options.type == 2) && options.type != 3) {
+			_html += 	'<div class="dialog-header">'+options.title+'<a href="javascript:;" class="dialog-close dialog-close-'+options.id+'" ><i class="fa fa-close font-16"></i></a></div>';
+		}
+			_html += 	'<div class="dialog-body">'+options.content+'</div>';
+		if (options.type != 2 && options.type != 3) {
+			_html +=	'<div class="dialog-footer dialog-footer-right">'+
+							'<div class="btn-group">';
+
+			if (options.confirmButton){
+				_html +=		'<a href="javascript:;" class="btn btn-primary dialog-confirm-'+options.id+'" >确认</a>';
+			}				
+			
+			if (options.cancelButton){
+				_html +=		'<a href="javascript:;" class="btn dialog-cancel-'+options.id+'">取消</a>';
+			}		
+				_html +=		 '</div>'+
+							'</div>'+
+						'</div>';
+		}			
+			
+		$(_html).appendTo(document.body).addClass('in');
+
+		_self.$element = $(_html);
+		_self.backdrop(options);
+		_self.bindEvent(options);
+
 	}
 
 	// 隐藏弹出框
-	Dialog.prototype.hide=function(options,elememt){
+	Dialog.prototype.hide=function(id){
 
-		var elememtId = elememt.attr('id');
-
-		this.$element.removeClass('in');
-		this.$element.off('click','[data-dialog="hide"]');
-
-		$('.dialog-backdrop-'+elememtId).length > 0 ? this.hideBackDrop(options,elememt) : '' ;
-
-
+		var elememtId = id || this.options.id;
+		$('#'+elememtId).removeClass('in').remove();
+		$('.dialog-backdrop-'+elememtId).length > 0 ? this.hideBackDrop(elememtId) : '' ;
 		
 	}
 
 	// 显示遮罩
-	Dialog.prototype.backdrop=function(options,elememt){
+	Dialog.prototype.backdrop=function(options){
 
-		var elememtId = elememt.attr('id');
-		
+		var elememtId = options.id;
+
 		if ($('.dialog-backdrop-'+elememtId).length <= 0) {
-			this.$backdrop = $('<div class="dialog-backdrop dialog-backdrop-'+elememtId+'" ></div>')
+			$('<div class="dialog-backdrop dialog-backdrop-'+elememtId+'" style="z-index:'+(dialogIndex-2)+'"></div>')
 			.appendTo(document.body)
 			.addClass('in');
 
 			$(document.body).addClass('dialog-open');
-			options.backdrop = false;
 		}
-		
 
 	}
 
 	// 隐藏遮罩
-	Dialog.prototype.hideBackDrop = function(options,elememt){
+	Dialog.prototype.hideBackDrop = function(elememtId){
 
-		var elememtId = elememt.attr('id');
+		var _self = this; 
 
 		$('.dialog-backdrop-'+elememtId).remove();
-		this.$backdrop = null;
-		options.backdrop = true;
 
-		$(document.body).removeClass('dialog-open');
+		if ($('.dialog-backdrop').length < 1) {
+			$(document.body).removeClass('dialog-open');
+		}
+		
 	}
 
 
 	// 绑定事件
-	Dialog.prototype.bindEvent=function(options,elememt){
+	Dialog.prototype.bindEvent=function(options){
 
-		var _self = this ;
-		var elememtId = elememt.attr('id');
-
-		// 点击文档外关闭弹框
-		$(document).on('click', '.dialog-backdrop-' + elememtId, function(event) {
-			_self.hide(options,elememt);
-		});
-
-		// 判断是否已经显示弹出框
-		_self.$element.on('click', '[data-dialog="hide"]', function(){
-			_self.hide(options,elememt);
-		});
-
-	}
-
-
-	$.fn.dialog = function(option) {
+		var _self = this,
+			elememtId = options.id;
 		
-		var element=this;
-		var options = $.extend($.fn.dialog.defaults, option);
-
-		return this.each(function () {
-          	new Dialog(this, options);
+		// 点击确认按钮
+		$('.dialog-confirm-'+elememtId+'').click(function(){
+			if (typeof _self.confirm == 'function') {
+				_self.confirm();
+			}
 		})
-		
-	}
 
-	// 默认配置
-	$.fn.dialog.defaults={
-		modal:'show',  // 显示或者隐藏弹出框
-		backdrop:true  // 是否出现遮罩
-	}
+		// 点击取消按钮
+		$('.dialog-cancel-'+elememtId+'').click(function(){
 
+			if (typeof _self.cancel == 'function') {
+				_self.cancel();
+				_self.hide(elememtId);
+			}
+		})
+
+		// 关闭操作
+		$('.dialog-backdrop-' + elememtId + ',.dialog-close-'+elememtId+'').click(function(){
+			_self.hide(elememtId);
+		})
+
+	}
 	
-
-	function stopEvent(e){
+	function stopEvent(e){_
 
 		if (!e) var e = window.event;
 		if (e.stopPropagation) { 
@@ -134,9 +168,9 @@
 
 	}
 
+	window.Dialog = Dialog; 
 
-
-}(window.jQuery);
+}(window,window.jQuery);
 
 /**
  * @authors H君
